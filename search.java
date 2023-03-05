e152b29d4123663689831e90d5f61f3cf9e2c751
Wasleyaar <92105465+Wasleyaar@users.noreply.github.com>
2023-03-05 21:06:25 +0530

    Add files via upload

---
+++ search.java
@@ -0,0 +1,129 @@
+package com.example.locationapp;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.locationapp.Model.Requests;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class search extends AppCompatActivity {
+
+    DatabaseReference ref;
+
+    ArrayList<Requests> list;
+    RecyclerView recyclerView;
+    SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+
+        ref= FirebaseDatabase.getInstance().getReference().child("Requests");
+        recyclerView= findViewById(R.id.rv);
+        searchView=findViewById(R.id.searchView);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ref!=null)
+        {
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists())
+                    {
+                        list=new ArrayList<>();
+                        for (DataSnapshot ds : dataSnapshot.getChildren())
+                        {
+                            list.add(ds.getValue(Requests.class));
+                            Collections.reverse(list);
+                        }
+
+
+                        AdapterClass adapterClass=new AdapterClass(list);
+                        recyclerView.setAdapter(adapterClass);
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(search.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+        if (searchView!=null)
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    search(s);
+                    return true;
+                }
+            });
+    }
+
+    private void search(String str) {
+        ArrayList<Requests> myList = new ArrayList<>();
+        for (Requests object : list)
+        {
+            if (object.getName().toLowerCase().contains(str.toLowerCase()))
+            {
+                myList.add(object);
+            }
+        }
+        AdapterClass adapterClass=new AdapterClass(myList);
+        recyclerView.setAdapter(adapterClass);
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(search.this);
+
+        // Set a title for alert dialog
+        builder.setTitle("Select your answer.");
+
+        // Ask the final question
+        builder.setMessage("Do you want to exit ForHer ?");
+
+        // Set the alert dialog yes button click listener
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        // Display the alert dialog on interface
+        dialog.show();
+    }
+}
